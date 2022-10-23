072d365e486ad41664f35f758b255125b5935267
Heeae <chromeee@daum.net>
2022-10-23 14:36:52 +0900

    입력받은 분 => 시간 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hoursSelector, minuteState } from "./atoms";
 
 function App() {
-  return <div>App</div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hoursSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); // string => number로 변경해주기
+  };
+
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" readOnly />
+    </div>
+  );
 }
 
 export default App;
