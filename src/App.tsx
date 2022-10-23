d5e79d73dc26e56b8de76c157aed679ddd665087
Heeae <chromeee@daum.net>
2022-10-23 14:50:03 +0900

    selector를 이용한 set, get 이용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hoursSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hoursSelector);
+  const [hours, setHours] = useRecoilState(hoursSelector);
 
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value); // string => number로 변경해주기
+  };
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
 
   return (
@@ -18,7 +22,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" readOnly />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,10 +5,16 @@ export const minuteState = atom({
   default: 0,
 });
 
-export const hoursSelector = selector({
+export const hoursSelector = selector<number>({
   key: "hours",
+  // 첫 번째 인자
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
+  },
+  // 두 번째 인자
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
